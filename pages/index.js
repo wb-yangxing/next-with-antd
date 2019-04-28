e67fa87cf96df791d5a80e1337df42181e06b034
yang_x <yang_x@ctrip.com>
2019-04-28 17:55:36 +0800

    init

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import {
+  Row,
+  Col,
+  Layout,
+} from 'antd';
+import "../style.less";
+export default () => (
+  <Layout>
+    <Row>
+      <Col span = {12}>col-12</Col>
+      <Col span = {12}>col-12</Col>
+    </Row>
+
+  </Layout>
+)
